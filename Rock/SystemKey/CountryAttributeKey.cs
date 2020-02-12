7e616b58a1dc9381589f1d153a6e7321c4b04e8c
David Leigh <to.david.leigh@gmail.com>
2020-02-13 01:43:26 +1100

    - Modified address validation to add a requirement level for individual address parts per Country.

---
+++ Rock/SystemKey/CountryAttributeKey.cs
@@ -0,0 +1,75 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+namespace Rock.SystemKey
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CountryAttributeKey
+    {
+
+        /// <summary>
+        /// The requirement level for the "Address Line 1" component of a postal address.
+        /// </summary>
+        public const string AddressLine1Requirement = "core_country_address_Line1Requirement";
+
+        /// <summary>
+        /// The requirement level for the "Address Line 2" component of a postal address.
+        /// </summary>
+        public const string AddressLine2Requirement = "core_country_address_Line2Requirement";
+
+        /// <summary>
+        /// The requirement level for the "City" component of a postal address.
+        /// </summary>
+        public const string AddressCityRequirement = "core_country_address_CityRequirement";
+
+        /// <summary>
+        /// The requirement level for the "Locality" component of a postal address.
+        /// </summary>
+        public const string AddressLocalityRequirement = "core_country_address_LocalityRequirement";
+
+        /// <summary>
+        /// The requirement level for the "State" component of a postal address.
+        /// </summary>
+        public const string AddressStateRequirement = "core_country_address_StateRequirement";
+
+        /// <summary>
+        /// The requirement level for the "Postal Code" component of a postal address.
+        /// </summary>
+        public const string AddressPostalCodeRequirement = "core_country_address_PostalCodeRequirement";
+
+        /// <summary>
+        /// The descriptive name for the "Locality" component of a postal address.
+        /// </summary>
+        public const string AddressLocalityLabel = "core_country_address_LocalityLabel";
+
+        /// <summary>
+        /// The descriptive name for the "City" component of a postal address.
+        /// </summary>
+        public const string AddressCityLabel = "CityLabel";
+
+        /// <summary>
+        /// The descriptive name for the "State" component of a postal address.
+        /// </summary>
+        public const string AddressStateLabel = "StateLabel";
+
+        /// <summary>
+        /// The descriptive name for the "Postal Code" component of a postal address.
+        /// </summary>
+        public const string AddressPostalCodeLabel = "PostalCodeLabel";
+    }
+}
